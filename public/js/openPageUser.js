aea5b3ea327a84bfa3b6e91632e0183f547c28df
Jony Reis <jonyreiscardoso@gmail.com>
2019-12-19 16:45:03 -0300

    refactoring challenge 04

--- public/js/openPageUser.js
+++ public/js/openPageUser.js
@@ -1,8 +1,24 @@
-const recipes = document.querySelectorAll('.itemIndex')
+function indexImg() {
+  const recipes = document.querySelectorAll('img')
 
-for (let recipe of recipes) {
-  recipe.addEventListener('click', function () {
-    const recipeIndex = recipe.getAttribute('id') 
-    window.location.href=`/recipes/${recipeIndex}`;
-  })
+  for (let recipe of recipes) {
+    recipe.addEventListener('click', function () {
+      const recipeIndex = recipe.getAttribute('id') 
+      window.location.href=`/recipes/${recipeIndex}`;
+    })
+  }
 }
+
+function indexH3() {
+  const recipes = document.querySelectorAll('h3')
+
+  for (let recipe of recipes) {
+    recipe.addEventListener('click', function () {
+      const recipeIndex = recipe.getAttribute('id') 
+      window.location.href=`/recipes/${recipeIndex}`;
+    })
+  }
+}
+
+indexImg()
+indexH3()
